1f7ad2f3b32c3ee558e0bfef15f2c7fa5f12727e
lucasnodine <lucasnodine@gmail.com>
2015-02-26 23:14:52 -0600

    Work towards billing.

---
+++ ViewModels/Billing/GroupInvoiceItemViewModel.cs
@@ -0,0 +1,48 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="GroupInvoiceViewModel.cs" company="Nodine Legal, LLC">
+// Licensed to Nodine Legal, LLC under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  Nodine Legal, LLC licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace OpenLawOffice.WebClient.ViewModels.Billing
+{
+    using System;
+    using AutoMapper;
+    using OpenLawOffice.Common.Models;
+    using System.Collections.Generic;
+
+    public class GroupInvoiceItemViewModel
+    {
+        public Matters.MatterViewModel Matter { get; set; }
+        public decimal ExpensesSum { get; set; }
+        public decimal FeesSum { get; set; }
+        public TimeSpan TimeSum { get; set; }
+        public decimal TimeSumMoney { get; set; }
+
+        public List<InvoiceTimeViewModel> Times { get; set; }
+        public List<InvoiceExpenseViewModel> Expenses { get; set; }
+        public List<InvoiceFeeViewModel> Fees { get; set; }
+
+        public GroupInvoiceItemViewModel()
+        {
+            Times = new List<InvoiceTimeViewModel>();
+            Expenses = new List<InvoiceExpenseViewModel>();
+            Fees = new List<InvoiceFeeViewModel>();
+        }
+    }
+}
---
+++ ViewModels/Billing/GroupInvoiceViewModel.cs
@@ -0,0 +1,55 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="GroupInvoiceViewModel.cs" company="Nodine Legal, LLC">
+// Licensed to Nodine Legal, LLC under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  Nodine Legal, LLC licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace OpenLawOffice.WebClient.ViewModels.Billing
+{
+    using System;
+    using AutoMapper;
+    using OpenLawOffice.Common.Models;
+    using System.Collections.Generic;
+
+    public class GroupInvoiceViewModel
+    {
+        public Guid? Id { get; set; }
+        public Contacts.ContactViewModel BillTo { get; set; }
+        public DateTime Date { get; set; }
+        public DateTime Due { get; set; }
+        public decimal Subtotal { get; set; }
+        public decimal TaxAmount { get; set; }
+        public decimal Total { get; set; }
+        public string ExternalInvoiceId { get; set; }
+        public string BillTo_NameLine1 { get; set; }
+        public string BillTo_NameLine2 { get; set; }
+        public string BillTo_AddressLine1 { get; set; }
+        public string BillTo_AddressLine2 { get; set; }
+        public string BillTo_City { get; set; }
+        public string BillTo_State { get; set; }
+        public string BillTo_Zip { get; set; }
+        public BillingGroupViewModel BillingGroup { get; set; }
+
+        public List<GroupInvoiceItemViewModel> Matters { get; set; }
+
+        public GroupInvoiceViewModel()
+        {
+            Matters = new List<GroupInvoiceItemViewModel>();
+        }
+    }
+}
